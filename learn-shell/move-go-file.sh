ab88a2e85cca7b9e4a67000ce596c1a92879387d
lzsbtot <378364317@qq.com>
2018-05-18 10:14:52 +0800

    Add example shell scripts

---
+++ learn-shell/move-go-file.sh
@@ -0,0 +1,37 @@
+
+#/usr/bin/bash
+
+if [ $# -eq 1 ]
+then
+    path=$1
+else
+    echo "Please provide the target directory!"
+    exit 1;
+fi
+
+
+echo target files are : `ls $path`
+cd $path
+echo current directory is : `pwd`
+
+read -p "Type y to mkdir for each file and move them to each directory: " input
+case $input in
+    [yY]) 
+    for file in `ls`
+    do 
+        ext=${file#*.}
+        if [ $ext == "go"]
+        then
+            dir=${file%%.*}
+            mkdir $dir
+            mv $file $dir
+        fi
+    done
+    echo "Done!"
+    echo target files are moved to each folder: `ls $path`
+    ;;
+    [nN])
+    echo "Do nothing! Quit"
+    exit 1;
+    ;;
+esac
