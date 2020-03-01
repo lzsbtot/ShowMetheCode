e9914db13bfc83adc734c68140e5340cafd827cf
rpc <378364317@qq.com>
2020-03-01 20:14:42 +0800

    simplify fee tcpdump

--- learn-shell/example-sh/tcpdump.sh
+++ learn-shell/example-sh/tcpdump.sh
@@ -7,10 +7,13 @@ usage="Script to capture the tcpdump.
     where:
         -h  Show this help text
         -c  test case name
-        -t  capture type: application level tcpdump(appl) or fee tcpdump(fee)"
+        -t  capture type: 
+                appl        application level tcpdump
+                fee         evip fee tcpdump
+                all         appl and fee"
 
 optFlag=0
-while getopts ':ht:c:' option:
+while getopts ':ht:c:' option;
 do
     case "$option" in
         h) echo "$usage"
@@ -21,13 +24,13 @@ do
                 TC=$OPTARG
                 (( optFlag++ ))
            else
-                printf "ERROR: CASE name can not be empty!" >&2
+                printf "ERROR: TEST CASE name can not be empty!" >&2
                 echo "$usage" >&2
                 exit 1
            fi
            ;;
         t) case $OPTARG in
-                appl|fee) TYPE=$OPTARG
+                appl|fee|all) TYPE=$OPTARG
                     (( optFlag++ ))
                     ;;
                 *) printf "ERROR: Invalid argument for option -p: %s\n\n" "$OPTARG" >&2
@@ -40,7 +43,7 @@ do
             echo "$usage" >&2
             exit 1
             ;;
-        \?) printf "ERROR:  Illegal option: -%s\n" "$OPTARG" >&2
+        \?) printf "ERROR: Illegal option: -%s\n" "$OPTARG" >&2
             echo "$usage" >&2
             exit 1
             ;;
@@ -54,59 +57,47 @@ then
     exit 1
 fi
 
-echo
-echo "Take ${TYPE} tcpdump for test case : ${TC}"
-echo
-
 DATETIME=`date +"%Y%m%d-T%H%M%S"`
-BASE_RES="/cluster/home/${DATETIME}-${TC}-${TYPE}"
+BASE_RES="/cluster/home/${DATETIME}-${TC}"
 PL_LIST=`cluster config -p | grep "payload PL" | awk '{print $4}'`
-# HOST_IP=`cluster config -p | grep "host all" | awk '{print $3}' | sed ':a;N;s/\n/ or /;ba'`
 HOST_IP=`cluster config -p | grep "host all" | awk '{print $3}' | xargs |sed 's/ / or /g'`
 
 function start-appl-tcpdump {
     for pl in ${PL_LIST}
     do
         ssh -t -t -f ${pl} tcpdump -i any host ${HOST_IP} -s 0 -w ${BASE_RES}-${pl}.pcap < /dev/null > /dev/null 2>&1
-        echo "${TYPE} tcpdump is running on ${pl} now"
+        echo "capture appl tcpdump on ${pl}"
     done
 }
 
-function fetch-fee-info {
+function start-fee-tcpdump {
     declare -A map=()
     for pl in ${PL_LIST}
     do
         FEE_LIST=`ssh ${pl} ip netns list | grep -i fee`
         if [ "${FEE_LIST}" ]
         then
-            echo "***************** fee running on ${pl}  ********************"
-            for fee in ${FEE_LIST}; do echo ${fee}; done
             map["${pl}"]="${FEE_LIST}"
+            for fee in ${map["${pl}"]}
+            do
+                INTERFACE=`ssh ${pl} ip netns exec ${fee} ifconfig | grep eth | awk '{print $1}'`
+                ssh -t -t -f ${pl} "ip netns exec ${fee} tcpdump -i ${INTERFACE} -s 0 -w ${BASE_RES}-fee-${pl}-${fee}.pcap" < /dev/null > /dev/null 2>&1
+                echo "capture fee tcpdump on ${pl} : ${fee}"
+            done
         fi
     done
 }
 
-function start-fee-tcpdump {
-    for pl in ${!map["${pl}"]}
-    do
-        for fee in ${map["${pl}"]}
-        do
-            INTERFACE=`ssh ${pl} ip netns exec ${fee} ifconfig | grep eth | awk '{print $1}'`
-            ssh -t -t -f ${pl} "ip netns exec ${fee} tcpdump -i ${INTERFACE} -s 0 -w ${BASE_RES}-${pl}-${fee}.pcap" < /dev/null > /dev/null 2>&1
-            echo "${TYPE} tcpdump is running on ${pl} ${fee} now"
-            echo
-        done
-    done 
-}
-
 case ${TYPE} in
     appl)
-        echo "*****************    start capturing now    ********************"
         start-appl-tcpdump
         ;;
     fee)
-        fetch-fee-info
-        echo "*****************    start capturing now    ********************"
+        start-fee-tcpdump
+        ;;
+    all)
+        start-appl-tcpdump
+        echo
         start-fee-tcpdump
         ;;
     *)
@@ -115,32 +106,25 @@ case ${TYPE} in
         ;;
 esac
 
-echo
-
 while :
 do
     read -n1 -p "Press [q/Q] to stop capturing:" KEY
     case ${KEY} in
-    q | Q)
-        echo
-        echo "Tcpdump will stop now!"
-        ps -ef | grep "${BASE_RES}" | grep -v grep | awk '{print $2}' | xargs kill -9
-        sleep 1
-        break
-    ;;
-    *)
-        echo
-        continue
-    ;;
+        q|Q)
+            echo -e "\n\ntcpdump will stop now!"
+            ps -ef | grep "${BASE_RES}" | grep -v grep | awk '{print $2}' | xargs kill -9
+            break
+            ;;
+        *)
+            echo
+            continue
+            ;;
     esac
 done
 
-echo
-echo "****************************************************************"
-echo
-echo "Result file:"
-tar -zcvf ${BASE_RES}.tar.gz `ls ${BASE_RES}*.pcap` --remove-files > /dev/null 2>&1
+echo -e "\nCompress Result pcap files:"
+tar -zcvf ${BASE_RES}.tar.gz `ls ${BASE_RES}*.pcap` --remove-files 2>/dev/null
+echo -e "\nDownload your result file from here:"
 ls -lh ${BASE_RES}.tar.gz
-echo
 echo "*************************** Done! ******************************"
 exit 0
